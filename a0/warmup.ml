852349746b4a64500ba999179db7606f7857d756
Oscar <os2304@columbia.edu>
2019-09-08 16:05:22 -0400

    init file

---
+++ a0/warmup.ml
@@ -0,0 +1,26 @@
+(** @author TODO My name (NetID) *)
+
+(* The code [failwith "Unimplemented"] below is just
+   a placeholder that raises an exception.  You should
+   replace it with your own implementation.  We will 
+   cover exceptions a little bit later in the course. *)
+
+(** TODO: document *)
+let valid_date y m d =
+  failwith "Unimplemented"
+
+(** TODO: document *)  
+let syr n =
+  failwith "Unimplemented"
+
+(** TODO: document *)
+let nacci n k =
+  failwith "Unimplemented"
+
+(* TODO: add assertions for testing *)
+
+(* TODO: set the value below to the number of hours
+   you spent working on this assignment, rounded to
+   the nearest integer *)
+   
+let hours_worked = -1
